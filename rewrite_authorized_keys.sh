ab54449aec787d69dd0cc423de7d2b159d1a8170
h-otter <h-otter@outlook.jp>
2019-06-28 17:10:11 +0900

    fix not-found user containing "-" in username

--- rewrite_authorized_keys.sh
+++ rewrite_authorized_keys.sh
@@ -15,7 +15,7 @@ then
   output="/dev/null"
 fi
 
-members=`curl -sS https://raw.githubusercontent.com/ictsc/ictsc-github-member/production/terraform.tfstate | grep github_team_membership.$team | sed -e 's/^.*-\(.*\)\".*$/\1/'`
+members=`curl -sS https://raw.githubusercontent.com/ictsc/ictsc-github-member/production/terraform.tfstate | grep github_team_membership.$team | sed -e 's/^.*ictsc[0-9]*-\(.*\)\".*$/\1/'`
 if [ -z "$members" ]
 then
   echo "members are not found, maybe $team do not exists"
@@ -25,7 +25,7 @@ fi
 echo "# These keys are imported members of Github Team $team defined by github.com/ictsc/ictsc-github-member" > $output
 for m in $members
 do
-  curl -sS https://github.com/$m.keys | grep -v "Not Found" | xargs -n1 -I{} echo {} $m@gh | tee --append $output
+  curl -sS https://github.com/$m.keys | xargs -n1 -I{} echo {} $m@gh | tee --append $output
 done
 
 if [ $output != "/dev/null" ]
