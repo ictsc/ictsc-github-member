f34b56f0b0d8c543f532cea0d4677b1cf9cc3dcb
x86taka <19627440+x86taka@users.noreply.github.com>
2022-11-24 22:23:41 +0900

    change provider version

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     github = {
       source  = "integrations/github"
-      version = "!= 5.9.0"
+      version = "5.7.0"
     }
   }
   required_version = ">= 0.13"
