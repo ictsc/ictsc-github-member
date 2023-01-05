c51866847628329896e950c46e719dd495039fe5
onokatio <onokatio@gmail.com>
2023-01-05 16:07:31 +0900

    migration

---
+++ move-team.tf
@@ -0,0 +1,54 @@
+moved {
+    from = github_team.ictsc4
+    to = github_team.team["ictsc4"]
+}
+
+moved {
+    from = github_team.ictsc5
+    to = github_team.team["ictsc5"]
+}
+
+moved {
+    from = github_team.ictsc6
+    to = github_team.team["ictsc6"]
+}
+
+moved {
+    from = github_team.ictsc7
+    to = github_team.team["ictsc7"]
+}
+
+moved {
+    from = github_team.ictsc8
+    to = github_team.team["ictsc8"]
+}
+
+moved {
+    from = github_team.ictsc9
+    to = github_team.team["ictsc9"]
+}
+
+moved {
+    from = github_team.ictsc2018
+    to = github_team.team["ictsc2018"]
+}
+
+moved {
+    from = github_team.ictsc2019
+    to = github_team.team["ictsc2019"]
+}
+
+moved {
+    from = github_team.ictsc2020
+    to = github_team.team["ictsc2020"]
+}
+
+moved {
+    from = github_team.ictsc2021
+    to = github_team.team["ictsc2021"]
+}
+
+moved {
+    from = github_team.ictsc2022
+    to = github_team.team["ictsc2022"]
+}
